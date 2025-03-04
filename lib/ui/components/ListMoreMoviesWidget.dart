8ae594f67d3ab676c0d4f68485b9fe68e31f84f7
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-03-04 22:17:04 +0700

    add filter movie on UI

--- lib/ui/components/ListMoreMoviesWidget.dart
+++ lib/ui/components/ListMoreMoviesWidget.dart
@@ -25,9 +25,11 @@ class ListMoreMoviesWidget extends StatefulWidget {
 
 class _ListMoreMoviesWidgetState extends State<ListMoreMoviesWidget> {
   List<Movies> data = [];
+  List<Movies> filteredData = []; // Danh sách sau khi lọc
   int currentPage = 1;
   bool isLoading = false;
   late ScrollController _scrollController;
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -35,113 +37,144 @@ class _ListMoreMoviesWidgetState extends State<ListMoreMoviesWidget> {
     _scrollController = ScrollController();
     _scrollController.addListener(_onScroll);
     fetchData();
+    _searchController.addListener(_filterMovies); // Lắng nghe thay đổi trong search
   }
 
   @override
   void dispose() {
     _scrollController.dispose();
+    _searchController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF121212),
-      appBar: AppBar(
-        title: const Text(
-          'Danh Sách Phim',
-          style: TextStyle(
-            color: Color(0xFFFFD700),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Color(0xFF121212),
-        elevation: 0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: GridView.builder(
-          controller: _scrollController,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 16,
-            mainAxisSpacing: 16,
-            childAspectRatio: 2 / 3,
-          ),
-          itemCount: data.length + (isLoading ? 1 : 0),
-          itemBuilder: (context, index) {
-            if (index == data.length) {
-              return Center(
-                child: CircularProgressIndicator(color: Color(0xFFFFD700)),
-              );
-            }
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailMovieWidget(
-                        data[index],
-                        widget.getDetailMoviesUseCase,
-                        widget.getDetailMoviesPresenter),
+      backgroundColor: const Color(0xFF121212),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          // AppBar với tiêu đề và thanh tìm kiếm
+          SliverAppBar(
+            backgroundColor: const Color(0xFF121212),
+            elevation: 0,
+            pinned: true, // Cố định khi scroll
+            title: const Text(
+              'Danh Sách Phim',
+              style: TextStyle(
+                color: Color(0xFFFFD700),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(60), // Chiều cao của thanh tìm kiếm
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: TextField(
+                  controller: _searchController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Tìm kiếm phim...',
+                    hintStyle: const TextStyle(color: Colors.grey),
+                    filled: true,
+                    fillColor: const Color(0xFF1E1E1E),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(color: Color(0xFFFFD700)),
+                    ),
+                    prefixIcon: const Icon(Icons.search, color: Color(0xFFFFD700)),
                   ),
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Color(0xFF1E1E1E),
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
-                    color: Color(0xFFFFD700),
-                    width: 1,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xFFFFD700).withOpacity(0.3),
-                      blurRadius: 10,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ClipRRect(
-                      borderRadius:
-                          const BorderRadius.vertical(top: Radius.circular(12)),
-                      child: Image.network(
-                        data[index].posterUrl,
-                        height: 180,
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                        loadingBuilder: (context, child, loadingProgress) {
-                          if (loadingProgress == null) {
-                            return child;
-                          }
-                          return shimmerLoadingEffect();
-                        },
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        data[index].name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFFFFD700),
-                        ),
-                      ),
-                    ),
-                  ],
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          ),
+          // Danh sách phim
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                childAspectRatio: 2 / 3,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                  if (index == filteredData.length && isLoading) {
+                    return const Center(
+                      child: CircularProgressIndicator(color: Color(0xFFFFD700)),
+                    );
+                  }
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailMovieWidget(
+                              filteredData[index],
+                              widget.getDetailMoviesUseCase,
+                              widget.getDetailMoviesPresenter),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF1E1E1E),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: const Color(0xFFFFD700),
+                          width: 1,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color(0xFFFFD700).withOpacity(0.3),
+                            blurRadius: 10,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                            child: Image.network(
+                              filteredData[index].posterUrl,
+                              height: 180,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                              loadingBuilder: (context, child, loadingProgress) {
+                                if (loadingProgress == null) {
+                                  return child;
+                                }
+                                return shimmerLoadingEffect();
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              filteredData[index].name,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFFFFD700),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                childCount: filteredData.length + (isLoading ? 1 : 0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -150,11 +183,11 @@ class _ListMoreMoviesWidgetState extends State<ListMoreMoviesWidget> {
     return Shimmer.fromColors(
       baseColor: Colors.grey[800]!,
       highlightColor: Colors.grey[500]!,
-      period: Duration(milliseconds: 500), // Tăng tốc độ nhấp nháy
+      period: const Duration(milliseconds: 500),
       child: Container(
         height: 180,
         width: double.infinity,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.black,
           shape: BoxShape.rectangle,
         ),
@@ -164,15 +197,14 @@ class _ListMoreMoviesWidgetState extends State<ListMoreMoviesWidget> {
 
   void _onScroll() {
     if (_scrollController.position.pixels >=
-            _scrollController.position.maxScrollExtent - 100 &&
+        _scrollController.position.maxScrollExtent - 100 &&
         !isLoading) {
       fetchData();
     }
   }
 
-  fetchData() async {
+  Future<void> fetchData() async {
     setState(() => isLoading = true);
-
     var requestData = GetMovieListRequestData(
         widget.slug, currentPage, APP_DEFAULT_ITEM_PER_PAGE_MORE);
     await widget.getMoviesUseCase.execute(requestData);
@@ -180,8 +212,22 @@ class _ListMoreMoviesWidgetState extends State<ListMoreMoviesWidget> {
 
     setState(() {
       data.addAll(newData);
+      filteredData = List.from(data); // Cập nhật filteredData ban đầu
       currentPage++;
       isLoading = false;
+    });
+  }
+
+  void _filterMovies() {
+    final query = _searchController.text.toLowerCase();
+    setState(() {
+      if (query.isEmpty) {
+        filteredData = List.from(data); // Nếu không tìm kiếm, hiển thị toàn bộ data
+      } else {
+        filteredData = data
+            .where((movie) => movie.name.toLowerCase().contains(query))
+            .toList(); // Lọc theo tên phim
+      }
     });
   }
 }
